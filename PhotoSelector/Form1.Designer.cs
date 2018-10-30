d8502d90d755649b90b71b7ed6f644ff9a9a5415
jmkasun <kasun.jm@gmail.com>
2018-10-31 00:16:32 +0530

    add

---
+++ PhotoSelector/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace PhotoSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCopied = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDesBrows = new System.Windows.Forms.Button();
+            this.btnSourceBrows = new System.Windows.Forms.Button();
+            this.txtDes = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblCopied);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnDesBrows);
+            this.groupBox1.Controls.Add(this.btnSourceBrows);
+            this.groupBox1.Controls.Add(this.txtDes);
+            this.groupBox1.Controls.Add(this.txtSource);
+            this.groupBox1.Location = new System.Drawing.Point(1, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1145, 62);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblCopied
+            // 
+            this.lblCopied.AutoSize = true;
+            this.lblCopied.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopied.ForeColor = System.Drawing.Color.Green;
+            this.lblCopied.Location = new System.Drawing.Point(712, 9);
+            this.lblCopied.Name = "lblCopied";
+            this.lblCopied.Size = new System.Drawing.Size(58, 16);
+            this.lblCopied.TabIndex = 2;
+            this.lblCopied.Text = "Copied";
+            this.lblCopied.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label2.Location = new System.Drawing.Point(590, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination Folder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(11, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Folder";
+            // 
+            // btnDesBrows
+            // 
+            this.btnDesBrows.Location = new System.Drawing.Point(1082, 27);
+            this.btnDesBrows.Name = "btnDesBrows";
+            this.btnDesBrows.Size = new System.Drawing.Size(57, 23);
+            this.btnDesBrows.TabIndex = 1;
+            this.btnDesBrows.Text = "Brows";
+            this.btnDesBrows.UseVisualStyleBackColor = true;
+            this.btnDesBrows.Click += new System.EventHandler(this.btnDesBrows_Click);
+            // 
+            // btnSourceBrows
+            // 
+            this.btnSourceBrows.Location = new System.Drawing.Point(510, 26);
+            this.btnSourceBrows.Name = "btnSourceBrows";
+            this.btnSourceBrows.Size = new System.Drawing.Size(57, 23);
+            this.btnSourceBrows.TabIndex = 1;
+            this.btnSourceBrows.Text = "Brows";
+            this.btnSourceBrows.UseVisualStyleBackColor = true;
+            this.btnSourceBrows.Click += new System.EventHandler(this.btnSourceBrows_Click);
+            // 
+            // txtDes
+            // 
+            this.txtDes.Location = new System.Drawing.Point(593, 29);
+            this.txtDes.Name = "txtDes";
+            this.txtDes.Size = new System.Drawing.Size(481, 20);
+            this.txtDes.TabIndex = 0;
+            this.txtDes.TextChanged += new System.EventHandler(this.txtDes_TextChanged);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(14, 29);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(490, 20);
+            this.txtSource.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "IMG_4524.JPG");
+            this.imageList1.Images.SetKeyName(1, "IMG_4525.JPG");
+            this.imageList1.Images.SetKeyName(2, "IMG_4526.JPG");
+            this.imageList1.Images.SetKeyName(3, "IMG_4527.JPG");
+            this.imageList1.Images.SetKeyName(4, "IMG_4528.JPG");
+            this.imageList1.Images.SetKeyName(5, "IMG_4529.JPG");
+            this.imageList1.Images.SetKeyName(6, "IMG_4530.JPG");
+            this.imageList1.Images.SetKeyName(7, "IMG_4531.JPG");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1145, 592);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1148, 673);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Image Selector";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDesBrows;
+        private System.Windows.Forms.Button btnSourceBrows;
+        private System.Windows.Forms.TextBox txtDes;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCopied;
+    }
+}
+
---
+++ PhotoSelector/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSelector
+{
+    public partial class Form1 : Form
+    {
+        List<string> imagePath = new List<string>();
+        int currentIndex = 0;
+        Image image = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSourceBrows_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSource.Text))
+            {
+                folderBrowserDialog1.SelectedPath = txtSource.Text;
+            }
+
+            folderBrowserDialog1.ShowDialog();
+            if (string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                return;
+
+            txtSource.Text = folderBrowserDialog1.SelectedPath;
+            txtDes.Text = Path.Combine(txtSource.Text, "Selected");
+
+            imagePath.Clear();
+
+            foreach (string imgPath in Directory.GetFiles(txtSource.Text).Where(x => Path.GetExtension(x.ToLower()) == ".jpg" || Path.GetExtension(x.ToLower()) == ".jpeg"))
+            {
+                imagePath.Add(imgPath);
+            }
+
+            loadImage();
+            pictureBox1.Select();
+        }
+
+        private void btnDesBrows_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtDes.Text))
+            {
+                folderBrowserDialog1.SelectedPath = txtDes.Text;
+            }
+
+            folderBrowserDialog1.ShowDialog();
+            txtDes.Text = folderBrowserDialog1.SelectedPath;
+            pictureBox1.Select();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Right:
+                        {
+                            pictureBox1.Select();
+                            if (imagePath.Count > currentIndex + 1)
+                            {
+                                currentIndex++;
+                                loadImage();
+
+                            }
+                            else
+                            {
+                                currentIndex = 0;
+                                loadImage();
+                            }
+
+
+
+                            break;
+                        }
+
+                    case Keys.Left:
+                        {
+                            pictureBox1.Select();
+                            if (currentIndex == 0)
+                            {
+                                currentIndex = imagePath.Count - 1;
+                                loadImage();
+                            }
+                            else
+                            {
+                                currentIndex--;
+                                loadImage();
+                            }
+
+
+                            break;
+                        }
+
+                    case Keys.Enter:
+                        {
+                            pictureBox1.Select();
+                            CopyImage();
+
+                            break;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+        private void CopyImage()
+        {
+            if (string.IsNullOrEmpty(txtDes.Text))
+            {
+                MessageBox.Show("Destination folder is empty");
+            }
+
+            string currentImage = imagePath[currentIndex];
+            string desFileName = Path.Combine(txtDes.Text, Path.GetFileName(currentImage));
+
+            if (File.Exists(desFileName))
+            {
+                ShowLabel(2);
+            }
+            else
+            {
+                File.Copy(currentImage, desFileName);
+                ShowLabel(1);
+
+            }
+
+        }
+
+        private void ShowLabel(int type)
+        {
+            if (type == 1)
+            {
+                lblCopied.Text = "Copied";
+                lblCopied.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblCopied.Text = "File already exists";
+                lblCopied.ForeColor = Color.Red;
+            }
+
+            timer1.Enabled = true;
+            timer1.Interval = 2000;
+            lblCopied.Visible = true;
+        }
+
+        private void loadImage()
+        {
+            if (imagePath.Count == 0)
+                return;
+
+            image = Image.FromFile(imagePath[currentIndex]);
+
+            // Compute thumbnail size.
+            Size thumbnailSize = GetThumbnailSize(image);
+
+            pictureBox1.Image = image;//.GetThumbnailImage(thumbnailSize.Width, thumbnailSize.Height, ThumbnailCallback, IntPtr.Zero);
+            GC.Collect();
+        }
+
+        private Size GetThumbnailSize(Image original)
+        {
+            // Maximum size of any dimension.
+            int maxPixels = Math.Max(pictureBox1.Width, pictureBox1.Height);
+
+            // Width and height.
+            int originalWidth = original.Width;
+            int originalHeight = original.Height;
+
+            // Compute best factor to scale entire image based on larger dimension.
+            double factor;
+            if (originalWidth > originalHeight)
+            {
+                factor = (double)maxPixels / originalWidth;
+            }
+            else
+            {
+                factor = (double)maxPixels / originalHeight;
+            }
+
+            // Return thumbnail size.
+            return new Size((int)(originalWidth * factor), (int)(originalHeight * factor));
+        }
+
+        public bool ThumbnailCallback()
+        {
+            return true;
+        }
+
+        private void txtDes_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtDes.Text))
+            {
+                if (!Directory.Exists(txtDes.Text))
+                {
+                    Directory.CreateDirectory(txtDes.Text);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblCopied.Visible = false;
+            timer1.Enabled = false;
+        }
+    }
+}
